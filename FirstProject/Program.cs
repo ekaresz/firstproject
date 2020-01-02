ae93ad5282eb9446176a30c4bbc1d2a8a54bc99d
ekaresz <carloserdodi@gmail.com>
2020-01-02 14:36:54 +0000

    Test

---
+++ FirstProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Car
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It can be driven forward");
+            Console.WriteLine("It can be driven backward");
+        }
+    }
+}
